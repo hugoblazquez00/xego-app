cae687fc40704102492252aa0a2a4bfde59fa63a
hugoblazquez00 <bghugo7@gmail.com>
2025-01-23 23:48:05 +0100

    first test version for the home page

---
+++ src/app/components/xegoCard.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const XegoCard = ({ xego }) => {
+  return (
+    <div className="border rounded-lg p-4 w-80 h-96 flex flex-col">
+      <div className="flex justify-between mb-2">
+        <span className="bg-gray-200 px-2 py-1 rounded text-sm">
+          {xego.difficulty}
+        </span>
+        <span className="bg-gray-100 px-2 py-1 rounded text-sm">
+          {xego.category}
+        </span>
+      </div>
+      
+      <h2 className="text-xl font-bold mb-2">{xego.title}</h2>
+      
+      <p className="text-gray-600 mb-4 flex-grow overflow-hidden">
+        {xego.description}
+      </p>
+      
+      <div className="mb-4">
+        <h3 className="font-semibold mb-2">Technologies:</h3>
+        <div className="flex flex-wrap gap-1">
+          {xego.technologies.map((tech, index) => (
+            <span 
+              key={index} 
+              className="bg-gray-100 px-1 py-0.5 rounded text-xs"
+            >
+              {tech}
+            </span>
+          ))}
+        </div>
+      </div>
+      
+      <div className="text-sm text-gray-500">
+        {xego.views} Views
+      </div>
+    </div>
+  );
+};
+
+export default XegoCard;
--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,8 +1,40 @@
+"use client";
+import { useEffect, useState } from 'react';
+import XegoCard from '@/app/components/xegoCard';
 
 export default function Home() {
+  const [xegoProjects, setXegoProjects] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchXegos = async () => {
+      try {
+        const response = await fetch('/api/xegos');
+        if (response.ok) {
+          const data = await response.json();
+          setXegoProjects(data);
+        } else {
+          throw new Error("Error fetching Xegos");
+        }
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false); // Terminamos la carga
+      }
+    };
+
+    fetchXegos();
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error}</div>;
+
   return (
-    <div>
-      HOla
+    <div className="grid grid-cols-2 gap-4">
+      {xegoProjects.map((xego, index) => (
+        <XegoCard key={index} xego={xego} />
+      ))}
     </div>
   );
 }
