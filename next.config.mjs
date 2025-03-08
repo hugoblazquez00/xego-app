7354d63ed84a97293511cf782aac17e8b2c3adbb
hugoblazquez00 <bghugo7@gmail.com>
2025-03-08 22:14:12 +0100

    Compilation of user's code move to the backend

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+      if (isServer) {
+        config.externals = config.externals || [];
+        config.externals.push(({ context, request }, callback) => {
+          if (request && request.includes("esbuild")) {
+            return callback(null, "commonjs " + request);
+          }
+          callback();
+        });
+      }
+      return config;
+    },
+  };
+  
+  export default nextConfig;
